f1afc0ee1803e2a1ed6ad49ebc68145beea271cf
Maxim Petrov <mail@maximpetrov.net>
2024-05-20 22:26:51 +0400

    Update

--- blogicum/blogicum/urls.py
+++ blogicum/blogicum/urls.py
@@ -16,6 +16,7 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 
+
 urlpatterns = [
     path('', include('blog.urls', namespace='homepage')),
     path('posts/', include('blog.urls', namespace='posts')),
